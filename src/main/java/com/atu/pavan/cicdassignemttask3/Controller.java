306c85af1613685be07df4c03f3a074efda3267a
PAVANKALYAN-L00179368 <145034367+PAVANKALYAN-L00179368@users.noreply.github.com>
2023-10-15 15:50:25 +0530

    Update Controller.java requestmapping

--- src/main/java/com/atu/pavan/cicdassignemttask3/Controller.java
+++ src/main/java/com/atu/pavan/cicdassignemttask3/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping("/")
+    @RequestMapping("/pavan")
     public String Hello(){
         return " <h1>Hi, I am Pavan Kalyan and my L Id id L00179368</h1>";
     }
