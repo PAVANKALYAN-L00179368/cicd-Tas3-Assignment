313af0e88bad5a1425a2b1923bc3b58f110b69ea
PAVANKALYAN-L00179368 <L00179368@atu.ie>
2023-10-20 02:20:44 +0530

    modified test cases in test folder

--- src/main/java/com/atu/pavan/cicdassignemttask3/Controller.java
+++ src/main/java/com/atu/pavan/cicdassignemttask3/Controller.java
@@ -1,11 +1,12 @@
 package com.atu.pavan.cicdassignemttask3;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping("/pavan")
+    @GetMapping("/pavan")
     public String Hello(){
         return " <h1>Hi, I am Pavan Kalyan and my L Id id L00179368</h1>";
     }
--- src/test/java/com/atu/pavan/cicdassignemttask3/CicdAssignmentTask3ApplicationTests.java
+++ src/test/java/com/atu/pavan/cicdassignemttask3/CicdAssignmentTask3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CicdAssignemtTask3ApplicationTests {
+class CicdAssignmentTask3ApplicationTests {
 
 	@Test
 	void contextLoads() {
